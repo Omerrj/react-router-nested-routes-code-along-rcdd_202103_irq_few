54163f8a154253bc30ca5d24eacd457cfe783ccf
Omer Sabah <omerrj99@gmail.com>
2021-06-21 12:21:15 +0000

    Automatically backed up by Learn

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import MoviesList from '../components/MoviesList';
-import from 
+import MovieShow from ""
 const MoviesPage = ({ match, movies }) => (
   <div>
     <MoviesList movies={movies} />
